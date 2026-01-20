b83e334a55c1841f2e6023dcfb57bae647cdb000
Hui Chen <alex.chenhui@gmail.com>
2026-01-20 20:41:38 +0800

    special token

---
+++ ch02/01_main-chapter-code/study/07.special_token.py
@@ -0,0 +1,64 @@
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = { i:s for s,i in vocab.items()}
+    
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [
+            item if item in self.str_to_int 
+            else "<|unk|>" for item in preprocessed
+        ]
+
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+        
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # Replace spaces before the specified punctuations
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
+        return text
+
+import re
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+    
+print("Total number of character:", len(raw_text))
+print(raw_text[:99])
+
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+print(preprocessed[:30])
+print(len(preprocessed))
+
+
+
+
+# text = "Hello, do you like tea. Is this-- a test?"
+
+# tokenizer.encode(text)
+
+
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+
+vocab = {token:integer for integer,token in enumerate(all_tokens)}
+for i, item in enumerate(list(vocab.items())[-5:]):
+    print(item)
+    
+tokenizer = SimpleTokenizerV2(vocab)
+
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+
+text = " <|endoftext|> ".join((text1, text2))
+
+print(text)
+
+ids = tokenizer.encode(text)
+print(ids)
+
+decoded_text = tokenizer.decode(ids)
+print(decoded_text)
