46e1741291468145082d67cf9f81cca8b574b47b
Hui Chen <alex.chenhui@gmail.com>
2026-01-21 18:57:49 +0800

    BFE tiktoken

--- ch02/01_main-chapter-code/study/08.BFE_tiktoken.py
+++ ch02/01_main-chapter-code/study/08.BFE_tiktoken.py
@@ -2,3 +2,18 @@ import tiktoken
 from importlib import metadata as importlib_metadata
 
 print("tiktoken version:", importlib_metadata.version("tiktoken"))
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+text = (
+    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
+     "of someunknownPlace."
+)
+
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+
+print(integers)
+
+strings = tokenizer.decode(integers)
+
+print(strings)
